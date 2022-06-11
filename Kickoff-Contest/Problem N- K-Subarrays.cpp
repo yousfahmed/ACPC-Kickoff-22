f6c3f89b9e72566ed31aeb1789af4285479e1b29
Abdeelaleem Ahmed <yousfahmed2@gmail.com>
2022-06-11 13:47:46 +0200

    Update Problem N- K-Subarrays.cpp

--- Kickoff-Contest/Problem N- K-Subarrays.cpp
+++ Kickoff-Contest/Problem N- K-Subarrays.cpp
@@ -18,20 +18,27 @@ int main() {
         int n, k;
         cin >> n >> k;
         int arr[n], ret[n];
+        map<int, int> mp;
         for (int &i: arr) cin >> i;
         vector<int> s(arr, arr + n);
         sort(all(s));
-        for (int i = 0; i < n; ++i)
-            ret[i] = upper_bound(all(s), arr[i]) - s.begin() - 1;
-
-        int ans = 0;
         for (int i = 0; i < n; ++i) {
+            if (mp.count(arr[i])) ret[i] = mp[arr[i]]++;
+            else {
+                mp[arr[i]] = lower_bound(all(s), arr[i]) - s.begin();
+                ret[i] = mp[arr[i]]++;
+            }
+        }
+        int ans = 0;
+        for (int i = n - 1; i >= 0; --i) {
             int cur = ret[i];
-            while (i < cur)
-                cur = max(cur, ret[i++]);
+            while (i > cur) {
+                i--;
+                if (i)cur = min(cur, ret[i]);
+            }
             ans++;
         }
-        cout << (ans >= k ? "YES\n" : "NO\n");
+        cout << (ans >= k ? "YES" : "NO");
     }
     return 0;
 }
